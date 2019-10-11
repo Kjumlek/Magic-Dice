5fd89ff85763e52960c35588fde823b30cef4d5a
Kjumlek <48591816+Kjumlek@users.noreply.github.com>
2019-10-12 01:12:47 +0200

    Update skybox.js

--- skybox.js
+++ skybox.js
@@ -29,18 +29,32 @@ function activateSky() {
     for (var i = 0; i < skyFaces.length; i++) {
         skyFaces[i].style.border = "none";
         skyFaces[i].style.opacity = "1";
+        skyFaces[i].style.border = "7px solid white";
+        skyFaces[i].style.opacity = "0.5";
+        skyFaces[i].style.backgroundColor = "transparent";
+        skyFaces[i].style.borderRadius = "0px";
+        skyFaces[i].style.transform = "translateX(0px) " + faceRotations[i];
     }
     for ( i = 0; i < skyPoints.length; i++) {
         skyPoints[i].style.opacity = "0";
     }
-    document.getElementsByClassName("front")[0].style.backgroundImage = "url('https://i.ibb.co/8b6tYf1/cubemap2.png')";
-    document.getElementsByClassName("left")[0].style.backgroundImage = "url('https://i.ibb.co/SJzq3pN/cubemap1.png')";
-    document.getElementsByClassName("right")[0].style.backgroundImage = "url('https://i.ibb.co/N28qy98/cubemap3.png')";
-    document.getElementsByClassName("back")[0].style.backgroundImage = "url('https://i.ibb.co/gS1VGfh/cubemap0.png')";
-    document.getElementsByClassName("top")[0].style.backgroundImage = "url('https://i.ibb.co/4TgVHwS/cubemap5.png')";
-    document.getElementsByClassName("floor")[0].style.backgroundImage = "url('https://i.ibb.co/wygNDwZ/cubemap4.png')";
-    document.getElementsByClassName("container")[0].style.perspective = "100px";
-    
     //konamiCode = [0];
+    document.querySelector('body').style.backgroundColor = "black";
+}
+
+
+
+    
+    document.addEventListener('keydown', function(f) {
+      if (40 == f.keyCode) {
+        
+        ii--;
+        animation = "loop " + ii + "s linear infinite";
+      }
+      console.log(f.keyCode);
+    });
+    
+    konamiCode = [0];
+    skyCode = [0];
     document.querySelector('body').style.backgroundColor = "black";
 }
